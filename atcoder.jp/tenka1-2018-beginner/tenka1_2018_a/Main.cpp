864fc7a0eafd2b4a67b5f59fcf1da929b8f1a25c
Nanashima <confi2100@gmail.com>
2022-06-02 21:00:33 +0900

    [AC] tenka1-2018-beginner tenka1_2018_a

---
+++ atcoder.jp/tenka1-2018-beginner/tenka1_2018_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    string s;
+    cin >> s;
+    if(s.size()==2) cout << s << endl;
+    else {
+        reverse(all(s));
+        cout << s << endl;
+    }
+    return 0;
+}
