a23b20cc0eca98f2d41fd591d429d15d8dc0f263
Nanashima <confi2100@gmail.com>
2022-06-18 21:00:56 +0900

    [AC] abc256 abc256_a

---
+++ atcoder.jp/abc256/abc256_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    ll n;
+    cin >> n;
+    ll ans=1;
+    rep(i,n) ans*=2;
+    cout << ans << endl; 
+    return 0;
+}
