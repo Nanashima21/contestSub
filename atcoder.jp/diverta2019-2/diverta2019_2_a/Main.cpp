64c67de58f6125a80c6711dd8af93541efdb9547
Nanashima <confi2100@gmail.com>
2022-06-30 21:01:38 +0900

    [AC] diverta2019-2 diverta2019_2_a

---
+++ atcoder.jp/diverta2019-2/diverta2019_2_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n,k;
+    cin >> n >> k;
+    if(k==1) cout << 0 << endl;
+    else cout << n-k << endl;
+    return 0;
+}
