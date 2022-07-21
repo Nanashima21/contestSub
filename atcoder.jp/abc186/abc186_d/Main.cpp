31347a08ece0200e0122c9acea058cc09a5f71bc
Nanashima <confi2100@gmail.com>
2022-07-21 21:26:57 +0900

    [AC] abc186 abc186_d

---
+++ atcoder.jp/abc186/abc186_d/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    sort(all(a));
+    ll tot=0;
+    rep(i,n) tot+=-a[i]*(n-i-1)+a[i]*i;
+    cout << tot << endl;
+    return 0;
+}
