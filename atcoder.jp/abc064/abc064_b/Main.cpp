c8ac7930d2613fe47483e7df496d2554eea054e1
Nanashima <confi2100@gmail.com>
2022-07-28 21:02:10 +0900

    [AC] abc064 abc064_b

---
+++ atcoder.jp/abc064/abc064_b/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    cout << *max_element(all(a))-*min_element(all(a)) << endl;
+    return 0;
+}
