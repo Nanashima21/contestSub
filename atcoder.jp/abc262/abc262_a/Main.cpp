b6be88a97015fdfe99616fce697179874258f0a7
Nanashima <confi2100@gmail.com>
2022-07-31 21:01:40 +0900

    [AC] abc262 abc262_a

---
+++ atcoder.jp/abc262/abc262_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int y;
+    cin >> y;
+    for(int i=y;;i++) if(i%4==2) {
+        cout << i << endl;
+        return 0;
+    }
+    return 0;
+}
