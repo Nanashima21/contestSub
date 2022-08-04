d3ed610de5a7972edac37b24217f9ad371950015
Nanashima <confi2100@gmail.com>
2022-08-04 21:00:46 +0900

    [AC] abc087 abc087_a

---
+++ atcoder.jp/abc087/abc087_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int x,a,b;
+    cin >> x >> a >> b;
+    x-=a;
+    cout << x%b << endl;
+    return 0;
+}
