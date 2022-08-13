31815776c942a6783ec098f8860cb436d30e157a
Nanashima <confi2100@gmail.com>
2022-08-13 21:01:47 +0900

    [AC] abc264 abc264_a

---
+++ atcoder.jp/abc264/abc264_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    string s="atcoder";
+    int l,r;
+    cin >> l >> r;
+    cout << s.substr(l-1,r-l+1) << endl;
+    return 0;
+}
