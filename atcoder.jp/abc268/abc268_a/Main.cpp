987dcd43e05896165dea34abd6d85f94753f400c
Nanashima <confi2100@gmail.com>
2022-09-14 16:59:25 +0900

    [AC] abc268 abc268_a

---
+++ atcoder.jp/abc268/abc268_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    set<int> se;
+    rep(i,5) {
+        int a;
+        cin >> a;
+        se.insert(a);
+    }
+    cout << (int)se.size() << endl;
+    return 0;
+}
