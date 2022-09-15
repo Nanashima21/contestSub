ecfc6411bf0767d49108b225904c7f04e39f6dcd
Nanashima <confi2100@gmail.com>
2022-09-15 21:00:41 +0900

    [AC] abc093 abc093_a

---
+++ atcoder.jp/abc093/abc093_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    string s;
+    cin >> s;
+    sort(all(s));
+    cout << ((s=="abc") ? "Yes" : "No") << endl;
+    return 0;
+}
