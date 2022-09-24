5ac79fc31d582f70009e32e8b454bd9fc80fa34a
Nanashima <confi2100@gmail.com>
2022-09-24 21:01:22 +0900

    [AC] abc270 abc270_a

---
+++ atcoder.jp/abc270/abc270_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    int c=0;
+    c=a|b;
+    cout << c << endl;
+    return 0;
+}
