f4b41eb46d7db3e1e50bceeeb17294dc2211cdfb
Nanashima <confi2100@gmail.com>
2022-10-22 21:01:02 +0900

    [AC] abc274 abc274_a

---
+++ atcoder.jp/abc274/abc274_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    double a,b;
+    cin >> a >> b;
+    printf("%.3f\n",b/a);
+    return 0;
+}
