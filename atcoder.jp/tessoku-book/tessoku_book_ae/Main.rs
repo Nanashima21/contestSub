2933b82be5b24939183dbc2bcb3e77424a9a77d8
Nanashima <confi2100@gmail.com>
2022-11-07 21:55:30 +0900

    [AC] tessoku-book tessoku_book_ae

---
+++ atcoder.jp/tessoku-book/tessoku_book_ae/Main.rs
@@ -0,0 +1,15 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use superslice::Ext;
+use proconio::{fastout, input};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+    };
+    println!("{}",n/3+n/5-n/15);
+}
