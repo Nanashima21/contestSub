baafdb9287631873b5290d2eb11b6ed3437c2a6e
Nanashima <confi2100@gmail.com>
2022-11-09 15:23:59 +0900

    [AC] tessoku-book tessoku_book_ay

---
+++ atcoder.jp/tessoku-book/tessoku_book_ay/Main.rs
@@ -0,0 +1,26 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use superslice::Ext;
+use proconio::{fastout, input};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[fastout]
+fn main() {
+    input! {
+        q: usize,
+    };
+    let mut stk=vec![];
+    for _ in 0..q {
+        input! { t: usize }
+        if t==1 {
+                input! { x: String }
+                stk.push(x);
+        } else if t==2 {
+            println!("{}",stk.last().unwrap());
+        } else {
+            stk.pop();
+        }
+    }
+}
