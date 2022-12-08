ccb1e649ad804949b6fb75463bffb55a69ed69fd
Nanashima <confi2100@gmail.com>
2022-12-08 21:38:12 +0900

    [AC] caddi2018b caddi2018_b

---
+++ atcoder.jp/caddi2018b/caddi2018_b/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n;
+    cin >> n;
+    bool flag=false;
+    rep(i,n) {
+        ll a;
+        cin >> a;
+        if(a%2==1) flag=true;
+    }
+    cout << (flag ? "first" : "second") << endl;
+    return 0;
+}
