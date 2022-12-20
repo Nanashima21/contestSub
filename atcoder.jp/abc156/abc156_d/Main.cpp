5a1aa774ecbbdda5199d7f03c9c4c21f421fa7d7
Nanashima <confi2100@gmail.com>
2022-12-20 16:30:41 +0900

    [AC] abc156 abc156_d

---
+++ atcoder.jp/abc156/abc156_d/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+const ll mod=1000000007;
+
+ll modpow(ll n,ll r) {
+    ll num=1;
+    while(r) {
+        if(r&1) num=num*n%mod;
+        n=n*n%mod;
+        r/=2;
+    }
+    return num;
+}
+
+ll comb(ll n,ll r) {
+    ll x=1,y=1;
+    for(ll i=0;i<r;i++) x=x*(n-i)%mod;
+    for(ll i=1;i<=r;i++) y=y*i%mod;
+    return x*modpow(y,mod-2)%mod;
+}
+
+int main() {
+    ll n,a,b;
+    cin >> n >> a >> b;
+    cout << (((modpow(2,n)-comb(n,a)+mod)%mod-comb(n,b)+mod)%mod-1+mod)%mod << endl;
+    return 0;
+}
