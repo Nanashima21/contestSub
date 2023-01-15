29123eaacaf71c9115b89236d9f77b9305d869d1
Nanashima <confi2100@gmail.com>
2023-01-15 17:54:19 +0900

    [AC] tessoku-book tessoku_book_bz

---
+++ atcoder.jp/tessoku-book/tessoku_book_bz/Main.rs
@@ -0,0 +1,22 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use superslice::Ext;
+use proconio::{fastout, input};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[allow(unused)]
+const INF: usize = std::usize::MAX/4;
+//const MOD: usize = 998_244_353;
+const MOD: usize = 1_000_000_007;
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+    };
+    println!("{}",n+m);
+}
+
