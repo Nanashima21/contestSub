7921dca0fd0dbc338a0bbff59704aceabb99a098
Nanashima <confi2100@gmail.com>
2023-01-15 17:55:54 +0900

    [AC] tessoku-book tessoku_book_ca

---
+++ atcoder.jp/tessoku-book/tessoku_book_ca/Main.rs
@@ -0,0 +1,28 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use superslice::Ext;
+use proconio::{fastout, input};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[allow(unused)]
+const INF: usize = std::usize::MAX/4;
+//const MOD: usize = 998_244_353;
+const MOD: usize = 1_000_000_007;
+
+#[fastout]
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+    };
+    let mut flag=false;
+    for i in a..=b {
+        if 100%i == 0 {
+            flag=true;
+        }
+    }
+    println!("{}",if flag {"Yes"} else {"No"});
+}
+
