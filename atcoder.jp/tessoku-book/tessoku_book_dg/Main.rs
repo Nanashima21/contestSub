5244597ab28105c396c3eb639b0020f667f83fe9
Nanashima <confi2100@gmail.com>
2023-01-21 00:48:05 +0900

    [AC] tessoku-book tessoku_book_dg

---
+++ atcoder.jp/tessoku-book/tessoku_book_dg/Main.rs
@@ -0,0 +1,31 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+use itertools::Itertools;
+use superslice::Ext;
+use proconio::{fastout, input};
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+
+#[allow(unused)]
+const INF: isize = std::isize::MAX/4;
+//const MOD: usize = 998_244_353;
+const MOD: usize = 1_000_000_007;
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        _x: usize,
+        _y: usize,
+        a: [usize; n],
+    };
+    let mut grundy = vec![];
+    for &ai in a.iter() {
+        grundy.push(ai%5/2);
+    }
+    let mut xor_sum = 0;
+    for &gi in grundy.iter() {
+        xor_sum^=gi;
+    } 
+    println!("{}",if xor_sum == 0 {"Second"} else {"First"});
+}
