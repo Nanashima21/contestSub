a6e7dd7062fb7d6fb83fb257c028b58e91562e82
Nanashima <confi2100@gmail.com>
2023-02-11 22:44:01 +0900

    [AC] abc289 abc289_a

---
+++ atcoder.jp/abc289/abc289_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    string s;
+    cin >> s;
+    for(char c:s) {
+        if(c=='0') cout << 1;
+        else cout << 0;
+    }
+    cout << endl;
+    return 0;
+}
