bcef918e91b137368c99090140664e43843917cf
Nanashima <confi2100@gmail.com>
2023-02-19 21:03:48 +0900

    [AC] abc290 abc290_b

---
+++ atcoder.jp/abc290/abc290_b/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n,k; string s;
+    cin >> n >> k >> s;
+    int cnt=0;
+    rep(i,n) {
+        if(s[i]=='o') {
+            cnt++;
+            if(cnt>k) s[i]='x';
+        } 
+    }
+    cout << s << endl;
+    return 0;
+}
