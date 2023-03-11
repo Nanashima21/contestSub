b968fae450a2aade1526968ec5c0fde91a1632be
Nanashima <confi2100@gmail.com>
2023-03-11 21:31:17 +0900

    [AC] abc293 abc293_d

---
+++ atcoder.jp/abc293/abc293_d/Main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+struct UnionFind {
+    vector<ll> v;
+    UnionFind(ll n=0): v(n,-1) {}
+    int find(ll x) {
+        if(v[x]<0) return x;
+        return v[x]=find(v[x]);
+    }
+    bool unite(ll x,ll y) {
+        x=find(x);
+        y=find(y);
+        if(x==y) return false;
+        if(v[x]>v[y]) swap(x,y);
+        v[x]+=v[y];
+        v[y]=x;
+        return true;
+    }
+    bool same(ll x,ll y) { return find(x)==find(y); }
+    ll size(ll x) { return -v[find(x)]; }
+    //UnionFind uf(n);
+};
+
+int main() {
+    ll n,m;
+    cin >> n >> m;
+    UnionFind uf(n);
+    int cnt=0,r=n;
+    rep(i,m) {
+        int a,c;
+        char b,d;
+        cin >> a >> b >> c >> d;
+        a--; c--;
+        if(!uf.same(a,c)) {
+            uf.unite(a,c);
+        } else {
+            cnt++;
+        }
+        r--;
+    }
+    cout << cnt << " " << r << endl;
+    return 0;
+}
