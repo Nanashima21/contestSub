ae1de1cd278b2e715133a102d7711bbed9f1fedd
Nanashima <confi2100@gmail.com>
2023-04-09 21:13:10 +0900

    [AC] abc297 abc297_c

---
+++ atcoder.jp/abc297/abc297_c/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a)  (a).begin(),(a).end()
+#define rall(a)  (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int h,w;
+    cin >> h >> w;
+    rep(i,h) {
+        string s;
+        cin >> s;
+        rep(j,w-1) {
+            if(s[j]=='T'&&s[j+1]=='T') {
+                s[j]='P';
+                s[j+1]='C';
+            } 
+        }
+        cout << s << endl;
+    }
+    return 0;
+}
