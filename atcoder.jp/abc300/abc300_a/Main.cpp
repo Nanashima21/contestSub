21f3443d10e84ed163f1828596c332ecfc3048f3
Nanashima <confi2100@gmail.com>
2023-04-29 21:01:26 +0900

    [AC] abc300 abc300_a

---
+++ atcoder.jp/abc300/abc300_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n,a,b;
+    cin >> n >> a >> b;
+    rep(i,n) {
+        int c;
+        cin >> c;
+        if(a+b==c) {
+            cout << i+1 << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
