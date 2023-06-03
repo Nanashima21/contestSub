e6edb7f2bfa38a763737a6a2fd2eff8f44a1ff5b
Nanashima <confi2100@gmail.com>
2023-06-03 21:03:37 +0900

    [AC] abc304 abc304_a

---
+++ atcoder.jp/abc304/abc304_a/Main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    vector<int> a(n);
+    ll idx=0,mi=INF;
+    rep(i,n) {
+        cin >> s[i] >> a[i];
+        if(mi>a[i]) {
+            idx=i;
+            mi=a[i];
+        }
+    }
+    for(int i=0;i<n;i++) cout << s[(idx+i)%n] << endl;
+
+    return 0;
+}
