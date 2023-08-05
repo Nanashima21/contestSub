ae72a8c784d853c6db937165fbea671f717c232a
Nanashima <confi2100@gmail.com>
2023-08-05 21:04:29 +0900

    [AC] abc313 abc313_a

---
+++ atcoder.jp/abc313/abc313_a/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<pl> p;
+    ll ma=0,p0=0;
+    rep(i,n) {
+        ll x;
+        cin >> x;
+        if(i==0) p0=x;
+        p.push_back({x,i});
+    }
+    sort(rall(p));
+    if(p[0].second==0) cout << 0 << endl;
+    else cout << p[0].first-p0+1 << endl;
+    return 0;
+}
