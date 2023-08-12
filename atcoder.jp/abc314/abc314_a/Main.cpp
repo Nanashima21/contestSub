2679f9b1383a5d06c676f6c3c27d44721d5b70b5
Nanashima <confi2100@gmail.com>
2023-08-12 21:01:37 +0900

    [AC] abc314 abc314_a

---
+++ atcoder.jp/abc314/abc314_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    string s = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    int n;
+    cin >> n;
+    cout << s.substr(0,n+2) << endl;
+    return 0;
+}
