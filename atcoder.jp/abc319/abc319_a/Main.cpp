8b24b39acaeccae052dfe37fc14f156ab9c10fdc
Nanashima <confi2100@gmail.com>
2023-09-09 21:04:39 +0900

    [AC] abc319 abc319_a

---
+++ atcoder.jp/abc319/abc319_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    string s;
+    cin >> s;
+    if(s[0]=='t') cout << 3858 << endl;
+    if(s[0]=='k') cout << 3679 << endl;
+    if(s[0]=='B') cout << 3658 << endl;
+    if(s[0]=='U') cout << 3648 << endl;
+    if(s[0]=='a') cout << 3638 << endl;
+    if(s[0]=='S') cout << 3630 << endl;
+    if(s[0]=='e') cout << 3613 << endl;
+    if(s[0]=='m') cout << 3555 << endl;
+    if(s[0]=='n') cout << 3516 << endl;
+    if(s[0]=='s') cout << 3481 << endl;
+    return 0;
+}
