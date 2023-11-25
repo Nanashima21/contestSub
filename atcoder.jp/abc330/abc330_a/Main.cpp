efc78d618af6b06e6dd27301e33d4bce119f7707
Nanashima <confi2100@gmail.com>
2023-11-25 21:00:43 +0900

    [AC] abc330 abc330_a

---
+++ atcoder.jp/abc330/abc330_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#define INF ((1LL<<62)-(1LL<<31))
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+typedef long long ll;
+typedef pair<ll,ll> pl;
+
+int main() {
+    int n,l;
+    cin >> n >> l;
+    int cnt=0;
+    rep(i,n) {
+        int a;
+        cin >> a;
+        if(a>=l) cnt++;
+    }
+    cout << cnt << endl;
+    return 0;
+}
